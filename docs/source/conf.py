901f43cb3d60c60fd8387a47b34996628e598e50
John Thornton <jthornton@gnipsel.com>
2019-03-07 09:15:01 -0600

    add pygments_style sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,8 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+# pygments_style = None
+pygments_style = sphinx
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -78,6 +79,7 @@ pygments_style = None
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
